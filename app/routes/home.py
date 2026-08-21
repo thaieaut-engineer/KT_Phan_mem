44b974460cfae5e230ebde46f53df2a97448c2c2
thaieaut-engineer <thaieaut@gmail.com>
2026-08-21 09:53:30 +0700

    Tạo giao diện trang chủ

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,22 +1,8 @@
-from flask import Blueprint
-from sqlalchemy import text
-from app import db
+from flask import Blueprint, render_template
 
 home_bp = Blueprint("home", __name__)
 
 
 @home_bp.route("/")
 def index():
-    try:
-        db.session.execute(text("SELECT 1"))
-        return """
-        <h1>🐾 PETSHOP</h1>
-        <h2>Flask: OK ✅</h2>
-        <h2>MySQL: Connected ✅</h2>
-        """
-    except Exception as e:
-        return f"""
-        <h1>🐾 PETSHOP</h1>
-        <h2>MySQL: Error ❌</h2>
-        <p>{e}</p>
-        """
+    return render_template("home.html")
---
+++ app/static/js/main.js
@@ -0,0 +1,1 @@
+console.log("PetShop JavaScript is running!");
